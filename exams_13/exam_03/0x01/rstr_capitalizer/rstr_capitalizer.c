f1df52d9707226b3e0876812b47f3a1df2d583f9
mirr <amir.exe@icloud.com>
2024-06-23 21:13:10 +0100

    tomorow_is_end_of_somting_and_bgn_of_somting

--- exams_13/exam_03/0x01/rstr_capitalizer/rstr_capitalizer.c
+++ exams_13/exam_03/0x01/rstr_capitalizer/rstr_capitalizer.c
@@ -14,28 +14,60 @@ void to_buffer(char *buf, char *str)
     buf[i] = '\0';
 }
 
-int main(int ac, char **av)
+void print_z(char *targ)
 {
-    char *x = "DeuxiEmE tEST uN PEU moinS  facile";
     int i = 0;
-    char buf[1024];
 
-    to_buffer(buf, x);
-
-    while (buf[i] != '\0')
+    while (targ[i] != '\0')
     {
-        if (buf[i + 1] == ' ')
-        {
-            if (buf[i] >= 97 && buf[i] <= 122)
-            {
-                buf[i] = buf[i] - 32;
-            }
-        }
-        else if (buf[i] >= 65 && buf[i] <= 90)
-        {
-            buf[i] = buf[i] + 32;
-        }
+        write(1, &targ[i], 1);
+
         i++;
     }
-    
+    write(1, "\n", 1);
+}
+
+int main(int ac, char **av)
+{
+
+    if (ac <= 1)
+    {
+        write(1, "\n", 1);
+        return (0);
+    }
+
+    char *x;
+    int con = ac;
+    int i = 0, bi = 1;
+    char buf[1024];
+
+    while (bi < con)
+    {
+        x = av[bi];
+        i = 0;
+        to_buffer(buf, x);
+
+        ///////////////////
+
+        while (buf[i] != '\0')
+        {
+            if (buf[i + 1] == ' ' || buf[i + 1] == '\0')
+            {
+                if (buf[i] >= 97 && buf[i] <= 122)
+                {
+                    buf[i] = buf[i] - 32;
+                }
+            }
+            else if (buf[i] >= 65 && buf[i] <= 90)
+            {
+                buf[i] = buf[i] + 32;
+            }
+            i++;
+        }
+        print_z(buf);
+
+        ///////////////////
+
+        bi++;
+    }
 }
